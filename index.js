e7cbfab804ee65e50659d52788fc532b7931903f
Dan Uhl <duhl@wayfair.com>
2020-01-24 08:21:53 -0500

    build urls before creating new promises

--- index.js
+++ index.js
@@ -21,28 +21,35 @@ const paths = [
 // we'll save using the url so take out any funky file/folder stuff (todo: look up official way to clean for saving regex)
 const cleanURL = url => url.replace('https://www.', '').replace('.com', '').replace(/[\/\.\?=]/g, '-');
 
+const concurrentPageCount = 5;
+const pagesIndex = -1;
+const getNextPage = pages => pages[(pagesIndex + 1) % concurrentPageCount];
+
+
 (async () => {
   try {
-    const promises = baseUrls.reduce((promises, base) =>
-      paths.reduce((promises, path) =>
-        promises.then(() =>
-          execute(`${base}${path}`)),
-        promises),
-      Promise.resolve());
+    const browser = await puppeteer.launch();
+    const pages = [];
+
+    // use a few concurrent pages to speed things up
+    const page = await browser.newPage();
+    const urls = baseUrls.flatMap(base => paths.map(path => `${base}${path}`));
+
+    const promises = urls.reduce((promises, url) =>
+      promises.then(() => execute(url, page)),
+      Promise.resolve()
+    );
     await promises;
+    await browser.close();
   } catch (e) {
     console.error(e);
     process.exit(1);
   }
 
-  async function execute(url) {
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
+  async function execute(url, page) {
     const timestamp = new Date().getTime();
     await page.goto(url);
     await page.waitFor(WAIT_TIME);
     await page.screenshot({path: `screenshots/${cleanURL(url)}-${timestamp}.png`, fullPage: true});
-    await page.emulateMedia('screen');
-    await browser.close();
   }
 })();
